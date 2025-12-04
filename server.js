a8e347f22f544f39c431c5e9fadbcf8258c83626
24gbl <ganesh.l@iksula.com>
2025-12-04 19:54:53 +0530

    remove-index-js-and-create-new-server-js-and-configure-apps-code

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// connect to Express JS
 const express = require('express');
 
+// USED to Express JS Funtions
 const app = express();
+
+// Used Port Number: 4000
 const port = 4000;
+
 app.use(express.json());
 
+// required the to-do list router
 const toDoListRouter = require('./routes/toDolist');
 
 
-// Home Page !!!
+// Home Page !!!!
 app.get('/', (req, res) => {
-  res.status(200).send('Home Page!!!!');
+  res.status(200).send('Home Page!!!!!!');
 });
 
 
@@ -17,5 +23,5 @@ app.get('/', (req, res) => {
 app.use('/todolists', toDoListRouter);
 
 app.listen(port, () => {
-  console.log(`DoList app listening on port http://localhost:${port}`);
+  console.log(`DoList app with Express JS listening on port http://localhost:${port}`);
 });
